911a4a1c66fea89ca0c3ed908e3d8692f73a7d33
amrr-jpeg <amirr-gab@mail.ru>
2026-06-17 01:46:16 +0300

    Add unique slug for content courses

--- app/Models/ContentCourse.php
+++ app/Models/ContentCourse.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class ContentCourse extends Model
 {
@@ -20,6 +21,25 @@ class ContentCourse extends Model
     protected $casts = [
         'is_published' => 'boolean',
     ];
+
+    public static function uniqueSlug(string $title): string
+    {
+        $baseSlug = Str::slug($title);
+
+        if ($baseSlug === '') {
+            $baseSlug = 'course';
+        }
+
+        $slug = $baseSlug;
+        $counter = 2;
+
+        while (self::where('slug', $slug)->exists()) {
+            $slug = $baseSlug . '-' . $counter;
+            $counter++;
+        }
+
+        return $slug;
+    }
 
     public function creator(): BelongsTo
     {
